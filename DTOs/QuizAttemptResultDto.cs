30748871dc1d9a7c502896f52eb3eb5aa21764fd
Sandali.js <sandalisandagomi@gmail.com>
2026-04-01 18:59:08 +0530

    feat: SA-111 add DetailedResults property to QuizAttemptResultDto for detailed grading outcomes

--- DTOs/QuizAttemptResultDto.cs
+++ DTOs/QuizAttemptResultDto.cs
@@ -8,4 +8,5 @@ public class QuizAttemptResultDto
     public int Score { get; set; }
     public int TotalQuestions { get; set; }
     public int XpEarned { get; set; }
+    public List<QuizAttemptDetailedResultDto> DetailedResults { get; set; } = [];
 }
